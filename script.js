9b611c1d0057b8ccfd41a5457cf4e6c6988b4b6a
7JOKER <113073172+JOKERSEV7@users.noreply.github.com>
2023-05-07 16:34:37 +0100

    Start working on the script.js by creating functions for (add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,12 @@
+const add = function(a, b) {
+    return a + b;
+};
+const subtract = function(a, b) {
+    return a - b;
+};
+const multiply = function(a, b) {
+    return a * b;
+};
+const divide = function(a, b) {
+    return a / b;
+}
